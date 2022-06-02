61a71e44cd0ea835d510d5848940c5dd1a4586f9
Ashley <ashl3yaoa@gmail.com>
2022-06-02 13:55:10 +0100

    Updated README.md with kanban and ERD, I also created two database that share a one to many realtionship

--- README.md
+++ README.md
@@ -8,6 +8,13 @@ methodologies and technologies that encapsulate all core modules
 covered during training.
 
 ## Application Design 
-
+>Entity Relationship Diagram (ERD)
 Coming up with a design that would showcase the different database relationships 
 ![Archecture sketch](https://user-images.githubusercontent.com/88076842/171422855-ca958668-93ff-4547-ac27-bd324a4a1b25.png)
+
+The suggested sketch is a draft of what the final version of the database would look like. However, for the scope of this assignment the *Minium Usable SubseT*(MUST) is two tables that share a one to many relationship.
+![artistSongERD](https://user-images.githubusercontent.com/88076842/171626661-1145f419-a98f-4c97-b98f-4ae6b9b5821d.png)
+
+>Trello Kanban Board
+I used Trello to create my kanban board, as it was suggested in the project brief.  I built off of the suggested template.
+![kanbanBoard](https://user-images.githubusercontent.com/88076842/171627633-bae70a2b-c081-4336-8519-bbdc9ac785e0.png)
--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy
-
+import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")    #"sqlite:///data.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
@@ -12,9 +13,16 @@ class Artist(db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
     stage_name=db.Column(db.String(30))
     email = db.Column(db.String(30))
+    songs = db.relationship('Song', backref = 'artistbr')
 
 class Songs(db.Model):
     song_id = db.Column(db.Integer, primary_key=True)
     song_name = db.Column(db.String(30))
     song_genre = db.Column(db.String(30))
-    artist_id = db.Column(db.Integer, )
+    artist_id = db.Column(db.Integer, db.ForeignKey('artist.user_id'), nullable = False)
+
+db.drop_all()
+db.create_all()
+
+if __name__ == '__name__':
+    app.run(debug = True, host='0.0.0.0')
